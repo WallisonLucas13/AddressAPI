67d3083fb7c53ba0f877dc726097faa3366c1151
WallisonLucas13 <lucaswalison136@gmail.com>
2024-08-16 17:39:39 -0300

    feat: cep model created

---
+++ src/main/java/com/example/api/Cep/domain/cep/models/CepModel.java
@@ -0,0 +1,21 @@
+package com.example.api.Cep.domain.cep.models;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class CepModel {
+
+    private String cep;
+    private String logradouro;
+    private String complemento;
+    private String unidade;
+    private String bairro;
+    private String localidade;
+    private String uf;
+    private String ibge;
+    private String gia;
+    private String ddd;
+    private String siafi;
+}
